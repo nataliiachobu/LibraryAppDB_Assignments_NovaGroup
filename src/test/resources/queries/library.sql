7920cf5fc8eb92062342afe20d1cf1bd34c98fc4
nataliiachobu <mebel2008@yahoo.com>
2023-08-07 11:30:18 -0700

    added US05 StepDefinitionsNC

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -19,3 +19,10 @@ select * from book_categories;
 select book_categories.name, count(*) from book_categories join books b on book_categories.id = b.book_category_id group by book_category_id
 having count(*) = (select max(book_count)
                    from (select count(*) as book_count from books group by book_category_id)as counts);
+
+#us02
+select count(*) from book_borrow
+where is_returned=0;
+
+#us03
+select name from book_categories;
