5140c726b8d409903c03ea4993cb8de14ee4615c
nataliiachobu <mebel2008@yahoo.com>
2023-08-07 11:14:44 -0700

    added US05 StepDefinitionsNC

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -1,3 +1,21 @@
-select count(id) from users; -- actual
+select *
+from books;
+select count(id)
+from users; -- actual
 
-select count(distinct id) from users; -- expected
+select count(distinct id)
+from users; -- expected
+
+select *
+from book_categories;
+
+select *
+from book_borrow;
+
+select *
+from books;
+select * from book_categories;
+
+select book_categories.name, count(*) from book_categories join books b on book_categories.id = b.book_category_id group by book_category_id
+having count(*) = (select max(book_count)
+                   from (select count(*) as book_count from books group by book_category_id)as counts);
